073cdc77f27755205797031d62fe83ddc44da6fe
Nhuengzii <32nhueng@gmail.com>
2023-05-12 22:08:11 +0700

    create welcome page and link to default landing page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:appfood2/pages/login.dart';
+import 'package:appfood2/pages/welcome.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: LoginPage(),
+      home: WelcomePage(),
     );
   }
 }
