a86d386aca5912b4f013366005c05c8a051a5286
Nhuengzii <32nhueng@gmail.com>
2023-05-13 18:24:18 +0700

    ling each topic with its detailed page

--- lib/pages/flag_nutrition.dart
+++ lib/pages/flag_nutrition.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:appfood2/pages/oil.dart';
+import 'package:appfood2/pages/rice_flour.dart';
+import 'package:appfood2/pages/vet_fruit.dart';
+import 'package:appfood2/pages/milk_meat.dart';
 
 class FlagNutrition extends StatelessWidget {
   const FlagNutrition({super.key});
@@ -56,20 +60,31 @@ class FlagNutrition extends StatelessWidget {
                                       padding:
                                           const EdgeInsetsDirectional.fromSTEB(
                                               0, 0, 0, 5.0),
-                                      child: Container(
-                                        width:
-                                            MediaQuery.of(context).size.width *
-                                                0.86,
-                                        height:
-                                            MediaQuery.of(context).size.height,
-                                        constraints: const BoxConstraints(
-                                          minWidth: 200,
-                                          maxHeight: 380,
+                                      child: GestureDetector(
+                                        onTap: () {
+                                          Navigator.of(context).push(
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      const RiceFlourPage()));
+                                        },
+                                        child: Container(
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.86,
+                                          height: MediaQuery.of(context)
+                                              .size
+                                              .height,
+                                          constraints: const BoxConstraints(
+                                            minWidth: 200,
+                                            maxHeight: 380,
+                                          ),
+                                          decoration: BoxDecoration(
+                                              color: Colors.greenAccent[100],
+                                              border: Border.all(
+                                                  color: Colors.black,
+                                                  width: 4)),
                                         ),
-                                        decoration: BoxDecoration(
-                                            color: Colors.greenAccent[100],
-                                            border: Border.all(
-                                                color: Colors.black, width: 4)),
                                       ),
                                     ),
                                   ),
@@ -86,39 +101,60 @@ class FlagNutrition extends StatelessWidget {
                                         mainAxisAlignment:
                                             MainAxisAlignment.center,
                                         children: [
-                                          Padding(
-                                            padding: const EdgeInsets.fromLTRB(
-                                                0, 0, 3, 5),
-                                            child: Container(
-                                              width: MediaQuery.of(context)
-                                                      .size
-                                                      .width *0.4,
-                                              height: MediaQuery.of(context)
-                                                  .size
-                                                  .height,
-                                              decoration: BoxDecoration(
-                                                  color:
-                                                      Colors.yellowAccent[100],
-                                                  border: Border.all(
-                                                      width: 4,
-                                                      color: Colors.black)),
+                                          GestureDetector(
+                                            onTap: () {
+                                              Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          const VetFruitPage()));
+                                            },
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.fromLTRB(
+                                                      0, 0, 3, 5),
+                                              child: Container(
+                                                width: MediaQuery.of(context)
+                                                        .size
+                                                        .width *
+                                                    0.4,
+                                                height: MediaQuery.of(context)
+                                                    .size
+                                                    .height,
+                                                decoration: BoxDecoration(
+                                                    color: Colors
+                                                        .yellowAccent[100],
+                                                    border: Border.all(
+                                                        width: 4,
+                                                        color: Colors.black)),
+                                              ),
                                             ),
                                           ),
-                                          Padding(
-                                            padding: const EdgeInsetsDirectional.fromSTEB(3, 0, 0, 5),
-                                            child: Container(
-                                              width: MediaQuery.of(context)
-                                                      .size
-                                                      .width *
-                                                  0.4,
-                                              height: MediaQuery.of(context)
-                                                  .size
-                                                  .height,
-                                              decoration: BoxDecoration(
-                                                  color: Colors.yellowAccent[200],
-                                                  border: Border.all(
-                                                      width: 4,
-                                                      color: Colors.black)),
+                                          GestureDetector(
+                                            onTap: () {
+                                              Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          const VetFruitPage()));
+                                            },
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsetsDirectional
+                                                      .fromSTEB(3, 0, 0, 5),
+                                              child: Container(
+                                                width: MediaQuery.of(context)
+                                                        .size
+                                                        .width *
+                                                    0.4,
+                                                height: MediaQuery.of(context)
+                                                    .size
+                                                    .height,
+                                                decoration: BoxDecoration(
+                                                    color: Colors
+                                                        .yellowAccent[200],
+                                                    border: Border.all(
+                                                        width: 4,
+                                                        color: Colors.black)),
+                                              ),
                                             ),
                                           ),
                                         ],
@@ -135,39 +171,59 @@ class FlagNutrition extends StatelessWidget {
                                         mainAxisAlignment:
                                             MainAxisAlignment.center,
                                         children: [
-                                          Padding(
-                                            padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 3, 5),
-                                            child: Container(
-                                              width: MediaQuery.of(context)
-                                                      .size
-                                                      .width *
-                                                  0.35,
-                                              height: MediaQuery.of(context)
-                                                      .size
-                                                      .height *
-                                                  0.35,
-                                              decoration: BoxDecoration(
-                                                  color: Colors.blueGrey[200],
-                                                  border: Border.all(
-                                                      width: 4,
-                                                      color: Colors.black)),
+                                          GestureDetector(
+                                            onTap: () {
+                                              Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          const MilkMeatPage()));
+                                            },
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsetsDirectional
+                                                      .fromSTEB(0, 0, 3, 5),
+                                              child: Container(
+                                                width: MediaQuery.of(context)
+                                                        .size
+                                                        .width *
+                                                    0.35,
+                                                height: MediaQuery.of(context)
+                                                        .size
+                                                        .height *
+                                                    0.35,
+                                                decoration: BoxDecoration(
+                                                    color: Colors.blueGrey[200],
+                                                    border: Border.all(
+                                                        width: 4,
+                                                        color: Colors.black)),
+                                              ),
                                             ),
                                           ),
-                                          Padding(
-                                            padding: const EdgeInsetsDirectional.fromSTEB(3, 0, 0, 5),
-                                            child: Container(
-                                              width: MediaQuery.of(context)
-                                                      .size
-                                                      .width *
-                                                  0.35,
-                                              height: MediaQuery.of(context)
-                                                  .size
-                                                  .height,
-                                              decoration: BoxDecoration(
-                                                  color: Colors.blueGrey[300],
-                                                  border: Border.all(
-                                                      width: 4,
-                                                      color: Colors.black)),
+                                          GestureDetector(
+                                            onTap: () {
+                                              Navigator.of(context).push(
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          const MilkMeatPage()));
+                                            },
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsetsDirectional
+                                                      .fromSTEB(3, 0, 0, 5),
+                                              child: Container(
+                                                width: MediaQuery.of(context)
+                                                        .size
+                                                        .width *
+                                                    0.35,
+                                                height: MediaQuery.of(context)
+                                                    .size
+                                                    .height,
+                                                decoration: BoxDecoration(
+                                                    color: Colors.blueGrey[300],
+                                                    border: Border.all(
+                                                        width: 4,
+                                                        color: Colors.black)),
+                                              ),
                                             ),
                                           ),
                                         ],
@@ -176,15 +232,24 @@ class FlagNutrition extends StatelessWidget {
                                   ),
                                   Expanded(
                                     flex: 27,
-                                    child: Container(
-                                      width: MediaQuery.of(context).size.width *
-                                          0.35,
-                                      height:
-                                          MediaQuery.of(context).size.height,
-                                      decoration: BoxDecoration(
-                                          color: Colors.green[400],
-                                          border: Border.all(
-                                              color: Colors.white, width: 4)),
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        Navigator.of(context).push(
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    const OilPage()));
+                                      },
+                                      child: Container(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.35,
+                                        height:
+                                            MediaQuery.of(context).size.height,
+                                        decoration: BoxDecoration(
+                                            color: Colors.green[400],
+                                            border: Border.all(
+                                                color: Colors.white, width: 4)),
+                                      ),
                                     ),
                                   )
                                 ],
