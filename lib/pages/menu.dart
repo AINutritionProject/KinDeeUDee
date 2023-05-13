6ebce520454069d85c642b3de64f0466007d3719
Nhuengzii <32nhueng@gmail.com>
2023-05-13 22:21:30 +0700

    create menu in find menu page and link to allfood page

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:appfood2/pages/camera.dart';
+import 'package:appfood2/pages/all_food.dart';
+import 'package:appfood2/pages/all_food.dart';
 
 class MenuPage extends StatelessWidget {
   const MenuPage({super.key});
@@ -19,9 +21,114 @@ class MenuPage extends StatelessWidget {
         ),
         appBar: AppBar(
           title: const Text("Find Menu"),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const AllFoodPage(
+                            type: "Fruit",
+                          )));
+                },
+                icon: const Icon(Icons.tv)),
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const AllFoodPage(type: "Flour")));
+                },
+                icon: const Icon(Icons.tv))
+          ],
         ),
-        body: const Center(
-          child: Text("MenuPage"),
+        body: Center(
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const AllFoodPage(type: "Fruit"),
+                      ),
+                    );
+                  },
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) =>
+                              const AllFoodPage(type: "Fruit")));
+                    },
+                    child: MenuTypeIcon(
+                      name: "ผลไม้",
+                      color: Colors.green.shade100,
+                      imageAssetPath: "assets/images/Fruit/fruit2.png",
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const AllFoodPage(type: "Flour"),
+                      ),
+                    );
+                  },
+                  child: MenuTypeIcon(
+                    name: "ข้าวแป้ง",
+                    color: Colors.yellow.shade300,
+                    imageAssetPath: "assets/images/RiceFlour/Flour2.png",
+                  ),
+                ),
+              ],
+            )
+          ]),
         ));
+  }
+}
+
+class MenuTypeIcon extends StatelessWidget {
+  const MenuTypeIcon(
+      {super.key,
+      required this.name,
+      required this.color,
+      required this.imageAssetPath});
+  final String name;
+  final Color color;
+  final String imageAssetPath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: color,
+          borderRadius: const BorderRadius.all(Radius.circular(30))),
+      margin: const EdgeInsets.only(left: 30, top: 30),
+      padding: const EdgeInsets.all(10),
+      height: 120,
+      width: 160,
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(30),
+          ),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              left: 48,
+              top: 10,
+              child: Container(
+                decoration: BoxDecoration(color: color),
+                child: Text(
+                  name,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            Center(child: Image.asset(imageAssetPath)),
+          ],
+        ),
+      ),
+    );
   }
 }
