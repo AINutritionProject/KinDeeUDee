06f8785b234263787eaa66f4d1f5e4c4bfc8139e
Nhuengzii <32nhueng@gmail.com>
2023-05-13 18:24:07 +0700

    create template for topic detailed page

---
+++ lib/pages/rice_flour.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RiceFlourPage extends StatelessWidget {
+  const RiceFlourPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Rice Flour Page")),
+    );
+  }
+}
