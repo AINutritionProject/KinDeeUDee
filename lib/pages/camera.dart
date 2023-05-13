c24467c74bb8d797137bd26e9922516a33958a33
Nhuengzii <32nhueng@gmail.com>
2023-05-13 18:04:23 +0700

    install image_picker package and template for get image from gallery

--- lib/pages/camera.dart
+++ lib/pages/camera.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:appfood2/pages/eat_confirm.dart';
 
 class CameraPage extends StatelessWidget {
   const CameraPage({super.key});
@@ -6,6 +8,22 @@ class CameraPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          XFile? image =
+              await ImagePicker().pickImage(source: ImageSource.gallery);
+          if (image == null) {
+            print("Not get image");
+            return;
+          }
+          if (!context.mounted) {
+            return;
+          }
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => EatConfirmPage(image: image)));
+        },
+        child: const Icon(Icons.image),
+      ),
       appBar: AppBar(
         title: const Text("Camera Page"),
       ),
---
+++ lib/pages/eat_confirm.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class EatConfirmPage extends StatelessWidget {
+  const EatConfirmPage({super.key, required this.image});
+  final XFile? image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 340,
+            width: 340,
+            child: Image.file(
+              File(image!.path),
+            ),
+          ),
+          const Text(
+            "คุณต้องการรับประทาน\n        อาหารชนิดนี้",
+            style: TextStyle(fontSize: 30),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("ใช่"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text("ไม่"),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
