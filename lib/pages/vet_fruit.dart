e7496f6520ef18634e11961d5f4201fed9e5bd0a
Nhuengzii <32nhueng@gmail.com>
2023-05-13 23:05:02 +0700

    create action button to quickly go to vet_ab

--- lib/pages/vet_fruit.dart
+++ lib/pages/vet_fruit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:appfood2/pages/vet_ab.dart';
 
 class VetFruitPage extends StatelessWidget {
   const VetFruitPage({super.key});
@@ -6,7 +7,29 @@ class VetFruitPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Vet Fruit Page")),
+      appBar: AppBar(
+        title: const Text("Vet Fruit Page"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const VetABPage(
+                        type: "A",
+                      )));
+            },
+            icon: const Icon(Icons.one_k),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const VetABPage(
+                        type: "B",
+                      )));
+            },
+            icon: const Icon(Icons.two_k),
+          )
+        ],
+      ),
     );
   }
 }
