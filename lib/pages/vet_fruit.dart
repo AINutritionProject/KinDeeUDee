79f6da856b94b7f002d9867a5031de53320828a4
Nhuengzii <32nhueng@gmail.com>
2023-05-13 18:24:07 +0700

    create template for topic detailed page

---
+++ lib/pages/vet_fruit.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class VetFruitPage extends StatelessWidget {
+  const VetFruitPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Vet Fruit Page")),
+    );
+  }
+}
