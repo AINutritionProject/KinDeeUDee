30f305a1593e2479c12457fe43324a43b140fcdb
Nhuengzii <32nhueng@gmail.com>
2023-05-13 22:49:24 +0700

    create vet_a and vet_b

---
+++ lib/pages/vet_a.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class VetAPage extends StatelessWidget {
+  const VetAPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Vet A page")),
+    );
+  }
+}
