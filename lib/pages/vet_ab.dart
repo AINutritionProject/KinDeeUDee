233af5a15ef8a136f6ebc283890c605140acc9a5
Nhuengzii <32nhueng@gmail.com>
2023-05-13 23:07:40 +0700

    show title of vet type

--- lib/pages/vet_ab.dart
+++ lib/pages/vet_ab.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 class VetABPage extends StatelessWidget {
-  const VetABPage({super.key});
+  const VetABPage({super.key, required this.type});
+  final String type;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Vet A page")),
+      appBar: AppBar(
+        title: Text(type == "A" ? "ผัก ก" : "ผัก ข"),
+      ),
     );
   }
 }
