5ef3a5defa31b44f116ce26c4fbbc86328e3a528
Nhuengzii <32nhueng@gmail.com>
2023-05-14 18:14:42 +0700

    create basic form

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -10,8 +10,61 @@ class RegisterPage extends StatelessWidget {
         title: const Text("Register"),
       ),
       body: const Center(
-        child: Text('Register Page'),
+        child: RegisterForm(),
       ),
+    );
+  }
+}
+
+class RegisterForm extends StatefulWidget {
+  const RegisterForm({super.key});
+
+  @override
+  State<RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormSlot(controller: _emailController, name: "อีเมล"),
+            TextFormSlot(
+              controller: _phoneNumberController,
+              name: "หมายเลขโทรศัพท์",
+            ),
+            TextFormSlot(
+              controller: _usernameController,
+              name: "ชื่อผู้ใช้",
+            ),
+            TextFormSlot(
+              controller: _passwordController,
+              name: "รหัสผ่าน",
+            )
+          ],
+        ));
+  }
+}
+
+class TextFormSlot extends StatelessWidget {
+  const TextFormSlot({super.key, required this.controller, required this.name});
+  final TextEditingController controller;
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(name),
+        TextFormField(controller: controller),
+      ],
     );
   }
 }
