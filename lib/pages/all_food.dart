47d5561e4b478a40d6dd3844d36fb57f0d654486
Nhuengzii <32nhueng@gmail.com>
2023-05-15 12:01:35 +0700

    create Basic Food Icon

--- lib/pages/all_food.dart
+++ lib/pages/all_food.dart
@@ -27,6 +27,13 @@ class AllFoodPage extends StatelessWidget {
               ),
             ),
           ),
+          const Center(
+            child: FoodIcons(
+                food: Food(
+                    name: "กล้วยไข่",
+                    type: "Fruit",
+                    imageAssetPath: "assets/images/Fruit/fruit2.png")),
+          )
         ],
       ),
     );
@@ -40,4 +47,71 @@ class MenuTypeIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(color: Colors.red, height: 30, width: 30);
   }
+}
+
+class FoodIcons extends StatelessWidget {
+  const FoodIcons({super.key, required this.food});
+  final Food food;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(30),
+        ),
+      ),
+      child: Stack(children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.red, borderRadius: BorderRadius.circular(40)),
+            child: const SizedBox(
+              width: 160,
+              height: 160,
+            ),
+          ),
+        ),
+        Positioned(
+          height: 320,
+          width: 420,
+          left: -97,
+          top: -95,
+          child: Image.asset(food.imageAssetPath),
+        ),
+        Positioned(
+          bottom: -0,
+          left: 20,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 5),
+            decoration: BoxDecoration(
+              color: Colors.green.shade100,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(30),
+              ),
+            ),
+            child: Text(
+              food.name,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+}
+
+class Food {
+  const Food({
+    required this.name,
+    required this.type,
+    required this.imageAssetPath,
+  });
+  final String name;
+  final String type;
+  final String imageAssetPath;
 }
