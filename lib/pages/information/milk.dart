58f819ca6b0e88921aa0d9076361b444b1f34b33
Nhuengzii <32nhueng@gmail.com>
2023-05-16 02:54:37 +0700

    decorate milk page

--- lib/pages/information/milk.dart
+++ lib/pages/information/milk.dart
@@ -10,11 +10,126 @@ class MilkPage extends StatefulWidget {
 }
 
 class _MilkPageState extends State<MilkPage> {
+  final int _milk_cout = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.yellow.shade50,
       appBar: AppBar(
         title: const Text('Milk Page'),
+      ),
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              margin: const EdgeInsets.only(top: 10),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  color: Colors.blue.shade200,
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Text(
+                "คุณดื่มนมหรือรับประทานผลิต\n      ภัณฑ์จากนมหรือไม่?",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Image.asset("assets/images/drink_eat/drink.png"),
+              Image.asset("assets/images/drink_eat/eat.png")
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                decoration: BoxDecoration(
+                  color: Colors.pink.shade200,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Text(
+                  "ไม่ดื่ม",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                decoration: BoxDecoration(
+                  color: Colors.pink.shade200,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Text(
+                  "ไม่ทาน",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                decoration: BoxDecoration(
+                  color: Colors.pink.shade200,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Text(
+                  "1แก้ว",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                decoration: BoxDecoration(
+                  color: Colors.pink.shade200,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Text(
+                  "ทาน",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            decoration: BoxDecoration(
+              color: Colors.pink.shade200,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: const Text(
+              "2แก้ว",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
