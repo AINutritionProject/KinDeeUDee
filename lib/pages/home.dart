f98986c5e81e1c61291289c0aaaf2b3efd4479c6
Nhuengzii <32nhueng@gmail.com>
2023-05-16 17:53:03 +0700

    solve merge conflict

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,6 +5,7 @@ import 'package:appfood2/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:appfood2/pages/login_success.dart';
+import 'package:appfood2/pages/eat_history.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
