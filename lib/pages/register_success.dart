2e9f2d2da11626bde21edeb4baa09f79cd27172a
Nhuengzii <32nhueng@gmail.com>
2023-05-16 20:31:58 +0700

    add button to sign out in register success page

--- lib/pages/register_success.dart
+++ lib/pages/register_success.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:appfood2/auth.dart';
 
 class RegisterSuccesPage extends StatelessWidget {
   const RegisterSuccesPage({super.key});
@@ -7,6 +8,12 @@ class RegisterSuccesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Register Success Page")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          Auth().signOut();
+        },
+        child: Icon(Icons.logout),
+      ),
     );
   }
 }
