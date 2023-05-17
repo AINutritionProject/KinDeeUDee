57f3fb9b13d748564b6f116c70fd25384ade7f18
Nhuengzii <32nhueng@gmail.com>
2023-05-17 21:11:26 +0700

    create ai_output page

---
+++ lib/pages/ai_output.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AIOutputPage extends StatelessWidget {
+  const AIOutputPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow.shade50,
+      appBar: AppBar(title: const Text("AI Output Page")),
+    );
+  }
+}
