fe9c74740774e78864679a982ad5975c01a48b4d
peebaku <baramee.254450@gmail.com>
2023-05-17 11:04:42 +0700

    full page register

--- lib/pages/register_success.dart
+++ lib/pages/register_success.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:appfood2/auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class RegisterSuccesPage extends StatelessWidget {
   const RegisterSuccesPage({super.key});
@@ -8,6 +9,14 @@ class RegisterSuccesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Register Success Page")),
+      body: Align(
+          alignment: Alignment.center,
+          child: FaIcon(
+            FontAwesomeIcons.circleCheck,
+            
+            color: Color.fromRGBO(65, 199, 175, 1),
+            size: 210,
+          )),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           Auth().signOut();
