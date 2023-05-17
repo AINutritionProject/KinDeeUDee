8176eb6b0e6e1ff5142336f29f4d78099d003c99
Nhuengzii <32nhueng@gmail.com>
2023-05-17 22:42:01 +0700

    create function to save eat history data in eat_confirm page

--- lib/pages/eat_confirm.dart
+++ lib/pages/eat_confirm.dart
@@ -2,10 +2,33 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:appfood2/pages/ai_output.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class EatConfirmPage extends StatelessWidget {
-  const EatConfirmPage({super.key, required this.image});
+  const EatConfirmPage(
+      {super.key, required this.image, this.name, this.unit, this.quantity});
   final XFile? image;
+  final String? name, unit;
+  final int? quantity;
+
+  Future<void> _saveEatHistory() async {
+    final uid = FirebaseAuth.instance.currentUser!.uid;
+    final nowTimestamp = DateTime.now().millisecondsSinceEpoch;
+    final filePath = "history/$uid/$nowTimestamp";
+    final storageRef = FirebaseStorage.instance.ref().child(filePath);
+    await storageRef.putFile(File(image!.path));
+    final String downloadUrl = await storageRef.getDownloadURL();
+    await FirebaseFirestore.instance.collection("eatHistory").add({
+      "uid": uid,
+      "timestamp": nowTimestamp,
+      "foodName": name ?? "yee",
+      "quantity": quantity ?? 999,
+      "unit": unit ?? "หิโมะ",
+      "foodPhoto": downloadUrl,
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +55,8 @@ class EatConfirmPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
+                  await _saveEatHistory();
                   Navigator.of(context).push(MaterialPageRoute(
                       builder: (context) => AIOutputPage(foodImage: image)));
                 },
