a6cd09119910414ef5a9bb4eed66259f617b9c7e
Nhuengzii <32nhueng@gmail.com>
2023-05-17 14:12:02 +0700

    create bmi page

---
+++ lib/pages/information/bmi.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class BMIPage extends StatelessWidget {
+  const BMIPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("BMI Page")),
+    );
+  }
+}
+
