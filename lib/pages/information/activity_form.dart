f94ef10edd9cbf14f02a7e0eeb2c0d43969d9358
PAFFx <jj2jom@gmail.com>
2023-05-25 17:24:10 +0700

    make next button

--- lib/pages/information/activity_form.dart
+++ lib/pages/information/activity_form.dart
@@ -65,165 +65,188 @@ class _ActivityFormBodyState extends State<ActivityFormBody> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            ActivityDisplay(
-              title: "1. กิจกรรมระดับเบามาก",
-              nameColor: const Color(0xFFFFD7D7),
-              frequencyColor: const Color(0xFFFFEBEB),
-              data: lightActivities,
-              setSelectedName: (String val) {
-                setState(() {
-                  activities[0].name = val;
-                });
-              },
-              setSelectedFrequency: (String val) {
-                activities[0].frequency = int.parse(val);
-              },
-            ),
-            ActivityDisplay(
-              title: "2. กิจกรรมระดับเบา",
-              nameColor: const Color(0xFFFFD7D7),
-              frequencyColor: const Color(0xFFFFEBEB),
-              data: lightActivities,
-              setSelectedName: (String val) {
-                setState(() {
-                  activities[1].name = val;
-                });
-              },
-              setSelectedFrequency: (String val) {
-                activities[1].frequency = int.parse(val);
-              },
-            ),
-            ActivityDisplay(
-              title: "3. กิจกรรมระดับปานกลาง",
-              nameColor: const Color(0xFFFFD7D7),
-              frequencyColor: const Color(0xFFFFEBEB),
-              data: lightActivities,
-              setSelectedName: (String val) {
-                setState(() {
-                  activities[2].name = val;
-                });
-              },
-              setSelectedFrequency: (String val) {
-                activities[2].frequency = int.parse(val);
-              },
-            ),
-            SizedBox(
-              width: 200,
-              child: FloatingActionButton.extended(
-                icon:
-                    const Icon(CupertinoIcons.plus_circle, color: Colors.black),
-                foregroundColor: const Color(0xFFFFD18B),
-                splashColor: const Color(0xFFFFD18B),
-                elevation: 0,
-                backgroundColor: const Color(0x00000000),
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return Dialog(
-                          backgroundColor: const Color(0x00FFFFFF),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.all(20.0),
-                                decoration: BoxDecoration(
-                                  color: const Color(0xFFFFD18B),
-                                  borderRadius: BorderRadius.circular(30),
-                                ),
-                                height: 200,
-                                child: Column(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Text(
-                                      "กิจกรรมที่ทำ",
-                                      style: TextStyle(fontSize: 22),
-                                    ),
-                                    const Padding(
-                                      padding: EdgeInsets.symmetric(
-                                          horizontal: 8.0, vertical: 10),
-                                      child: TextField(
-                                        keyboardType: TextInputType.text,
-                                        style: TextStyle(fontSize: 18),
-                                        decoration: InputDecoration(
-                                            contentPadding:
-                                                EdgeInsets.symmetric(
-                                                    vertical: 0,
-                                                    horizontal: 20),
-                                            border: OutlineInputBorder(
-                                              borderSide: BorderSide.none,
-                                            ),
-                                            filled: true,
-                                            fillColor: Colors.white,
-                                            hintText: "พาสุนัขไปเดินเล่น",
-                                            hintStyle:
-                                                TextStyle(color: Colors.red)),
-                                      ),
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: [
-                                        const Text("จำนวน"),
-                                        SmallDropDown(
-                                            data: frequency,
-                                            setSelectedItem: (String val) {
-                                              setState(() {
-                                                tempActivity.frequency =
-                                                    int.parse(val);
-                                              });
-                                            }),
-                                        const Text("ครั้ง/สัปดาห์"),
-                                      ],
-                                    ),
-                                  ],
-                                ),
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          ActivityDisplay(
+            title: "1. กิจกรรมระดับเบามาก",
+            nameColor: const Color(0xFFFFD7D7),
+            frequencyColor: const Color(0xFFFFEBEB),
+            data: lightActivities,
+            setSelectedName: (String val) {
+              setState(() {
+                activities[0].name = val;
+              });
+            },
+            setSelectedFrequency: (String val) {
+              activities[0].frequency = int.parse(val);
+            },
+          ),
+          ActivityDisplay(
+            title: "2. กิจกรรมระดับเบา",
+            nameColor: const Color(0xFFFFD7D7),
+            frequencyColor: const Color(0xFFFFEBEB),
+            data: lightActivities,
+            setSelectedName: (String val) {
+              setState(() {
+                activities[1].name = val;
+              });
+            },
+            setSelectedFrequency: (String val) {
+              activities[1].frequency = int.parse(val);
+            },
+          ),
+          ActivityDisplay(
+            title: "3. กิจกรรมระดับปานกลาง",
+            nameColor: const Color(0xFFFFD7D7),
+            frequencyColor: const Color(0xFFFFEBEB),
+            data: lightActivities,
+            setSelectedName: (String val) {
+              setState(() {
+                activities[2].name = val;
+              });
+            },
+            setSelectedFrequency: (String val) {
+              activities[2].frequency = int.parse(val);
+            },
+          ),
+          SizedBox(
+            width: 200,
+            child: FloatingActionButton.extended(
+              icon: const Icon(CupertinoIcons.plus_circle, color: Colors.black),
+              foregroundColor: const Color(0xFFFFD18B),
+              splashColor: const Color(0xFFFFD18B),
+              elevation: 0,
+              backgroundColor: const Color(0x00000000),
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Dialog(
+                        backgroundColor: const Color(0x00FFFFFF),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(20.0),
+                              decoration: BoxDecoration(
+                                color: const Color(0xFFFFD18B),
+                                borderRadius: BorderRadius.circular(30),
                               ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 20.0),
-                                child: ElevatedButton(
-                                    style: ButtonStyle(
-                                      shape: MaterialStatePropertyAll(
-                                          RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(20),
-                                      )),
-                                      backgroundColor:
-                                          const MaterialStatePropertyAll(
-                                              Color(0xFF7D90F3)),
+                              height: 200,
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    "กิจกรรมที่ทำ",
+                                    style: TextStyle(fontSize: 22),
+                                  ),
+                                  const Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 8.0, vertical: 10),
+                                    child: TextField(
+                                      keyboardType: TextInputType.text,
+                                      style: TextStyle(fontSize: 18),
+                                      decoration: InputDecoration(
+                                          contentPadding: EdgeInsets.symmetric(
+                                              vertical: 0, horizontal: 20),
+                                          border: OutlineInputBorder(
+                                            borderSide: BorderSide.none,
+                                          ),
+                                          filled: true,
+                                          fillColor: Colors.white,
+                                          hintText: "พาสุนัขไปเดินเล่น",
+                                          hintStyle:
+                                              TextStyle(color: Colors.red)),
                                     ),
-                                    onPressed: () {},
-                                    child: const Padding(
-                                      padding: EdgeInsets.symmetric(
-                                          vertical: 8.0, horizontal: 13),
-                                      child: Text(
-                                        "เพิ่ม",
-                                        style: TextStyle(fontSize: 20),
-                                      ),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      const Text("จำนวน"),
+                                      SmallDropDown(
+                                          data: frequency,
+                                          setSelectedItem: (String val) {
+                                            setState(() {
+                                              tempActivity.frequency =
+                                                  int.parse(val);
+                                            });
+                                          }),
+                                      const Text("ครั้ง/สัปดาห์"),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 20.0),
+                              child: ElevatedButton(
+                                  style: ButtonStyle(
+                                    shape: MaterialStatePropertyAll(
+                                        RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
                                     )),
-                              )
-                            ],
-                          ),
-                        );
-                      });
+                                    backgroundColor:
+                                        const MaterialStatePropertyAll(
+                                            Color(0xFF7D90F3)),
+                                  ),
+                                  onPressed: () {},
+                                  child: const Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 8.0, horizontal: 13),
+                                    child: Text(
+                                      "เพิ่ม",
+                                      style: TextStyle(fontSize: 20),
+                                    ),
+                                  )),
+                            )
+                          ],
+                        ),
+                      );
+                    });
+              },
+              label: const Text(
+                "เพิ่มกิจกรรม",
+                style: TextStyle(color: Colors.black, fontSize: 22),
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              margin: const EdgeInsets.only(top: 20),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor:
+                        const MaterialStatePropertyAll(Color(0xFFED7E7E)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ))),
+                onPressed: () {
+                  setState(() {
+                    // Navigator.of(context).push(MaterialPageRoute(
+                    //     builder: (context) => const ActivitySummary()));
+                  });
                 },
-                label: const Text(
-                  "เพิ่มกิจกรรม",
-                  style: TextStyle(color: Colors.black, fontSize: 22),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 3, horizontal: 15),
+                  child: Text(
+                    "ถัดไป",
+                    style: TextStyle(fontSize: 32),
+                  ),
                 ),
               ),
-            )
-          ],
-        ));
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
