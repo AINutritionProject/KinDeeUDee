466719e6fa12b3d2d0ef180070f53e3029735dac
peebaku <baramee.254450@gmail.com>
2023-05-27 01:55:15 +0700

    remove appbar in save success page

--- lib/pages/information/save_data_success.dart
+++ lib/pages/information/save_data_success.dart
@@ -9,73 +9,73 @@ class SaveDataSuccessPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Register Success Page")),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(top: 160),
-                child: FaIcon(
-                  FontAwesomeIcons.circleCheck,
-                  color: Color.fromRGBO(65, 199, 175, 1),
-                  size: 190,
-                ),
-              ),
-              const Padding(
-                  padding: EdgeInsets.only(top: 20),
-                  child: Align(
-                      alignment: Alignment.center,
-                      child: Text("บันทึกสำเร็จ",
-                          style: TextStyle(
-                              color: Color.fromRGBO(118, 183, 68, 1),
-                              fontSize: 35,
-                              fontWeight: FontWeight.w600)))),
-              Padding(
-                padding: const EdgeInsets.only(top: 50, left: 53, right: 30),
-                child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => const HomePage()));
-                    },
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromRGBO(178, 237, 255, 1),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(40),
-                        )),
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: double.infinity,
-                      height: 80,
-                      child: const Row(
-                        children: [
-                          Expanded(
-                              flex: 1,
-                              child: Icon(
-                                Icons.arrow_circle_right_outlined,
-                                size: 49,
-                              )),
-                          Expanded(
-                            flex: 6,
-                            child: Padding(
-                                padding: EdgeInsets.only(bottom: 10),
-                                child: Align(
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    "เข้าสู่เมนูหลัก",
-                                    style: TextStyle(
-                                        fontSize: 36,
-                                        fontWeight: FontWeight.w600,
-                                        color: Colors.black),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                )),
-                          )
-                        ],
-                      ),
-                    )),
-              ),
-            ],
+        body: SafeArea(
+            child: SingleChildScrollView(
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 160),
+            child: FaIcon(
+              FontAwesomeIcons.circleCheck,
+              color: Color.fromRGBO(65, 199, 175, 1),
+              size: 190,
+            ),
           ),
-        ));
+          const Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Align(
+                  alignment: Alignment.center,
+                  child: Text("บันทึกสำเร็จ",
+                      style: TextStyle(
+                          color: Color.fromRGBO(118, 183, 68, 1),
+                          fontSize: 35,
+                          fontWeight: FontWeight.w600)))),
+          Padding(
+            padding: const EdgeInsets.only(top: 50, left: 53, right: 30),
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const HomePage()));
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(178, 237, 255, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    )),
+                child: Container(
+                  alignment: Alignment.center,
+                  width: double.infinity,
+                  height: 80,
+                  child: const Row(
+                    children: [
+                      Expanded(
+                          flex: 1,
+                          child: Icon(
+                            Icons.arrow_circle_right_outlined,
+                            size: 49,
+                          )),
+                      Expanded(
+                        flex: 6,
+                        child: Padding(
+                            padding: EdgeInsets.only(bottom: 10),
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                "เข้าสู่เมนูหลัก",
+                                style: TextStyle(
+                                    fontSize: 36,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.black),
+                                textAlign: TextAlign.center,
+                              ),
+                            )),
+                      )
+                    ],
+                  ),
+                )),
+          ),
+        ],
+      ),
+    )));
   }
 }
