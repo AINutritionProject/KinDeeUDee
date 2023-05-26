506f646a70058e61e2f03f48fd063dd1dd5abc05
Nhuengzii <32nhueng@gmail.com>
2023-05-26 23:50:47 +0700

    create `save_data_success` page and link its

---
+++ lib/pages/information/save_data_success.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SaveDataSuccessPage extends StatelessWidget {
+  const SaveDataSuccessPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Save Data success page")),
+    );
+  }
+}
