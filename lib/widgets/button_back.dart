8b4148777c40c0f840998c6a0776c6d053539862
mahasachan <mahasachan.sa@ku.th>
2023-05-31 01:18:34 +0700

    Widget Button back How to use in discord :)

---
+++ lib/widgets/button_back.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+
+class ButtonBack extends StatelessWidget {
+  const ButtonBack({
+    super.key,
+    this.sizeCircle, 
+    this.color, 
+    this.colorCircle, 
+    this.size
+  });
+
+  final double? sizeCircle;
+  final Color? colorCircle;
+  final Color? color;
+  final double? size;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => {
+          Navigator.pop(context)
+      },
+      child: Container(
+        width: sizeCircle ?? 50,
+        height:  sizeCircle ?? 50,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(60),
+          color: colorCircle ?? Colors.white
+        ),
+        child: Icon(Icons.chevron_left, size: size ?? 45, color: color ?? Colors.black,),
+      ),
+    );
+  }
+}
