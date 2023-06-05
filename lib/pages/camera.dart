156c427260ce128aef4f572a6b2a2bd8fb1c63b7
Nhuengzii <32nhueng@gmail.com>
2023-06-05 23:23:01 +0700

    add red virtical line

--- lib/pages/camera.dart
+++ lib/pages/camera.dart
@@ -100,52 +100,66 @@ class _CameraPageState extends State<CameraPage> {
                     child: Center(
                       child: CameraPreview(
                         camController,
-                        child: Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                GestureDetector(
-                                  onTap: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: Container(
-                                    margin: const EdgeInsets.only(left: 10),
-                                    child: const Text(
-                                      "ออก",
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 30),
+                        child: Stack(children: [
+                          Center(
+                            child: Container(
+                              color: Colors.red,
+                              child: SizedBox(
+                                height: MediaQuery.of(context).size.height,
+                                width:
+                                    MediaQuery.of(context).size.width * 0.005,
+                              ),
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  GestureDetector(
+                                    onTap: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Container(
+                                      margin: const EdgeInsets.only(left: 10),
+                                      child: const Text(
+                                        "ออก",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 30),
+                                      ),
                                     ),
                                   ),
-                                ),
-                                IconButton(
-                                    onPressed: () {
-                                      if (_flashMode == FlashMode.off) {
-                                        _flashMode = FlashMode.torch;
-                                      } else {
-                                        _flashMode = FlashMode.off;
-                                      }
-                                      camController.setFlashMode(_flashMode);
-                                    },
-                                    icon: const Icon(
-                                      Icons.flash_auto_sharp,
-                                      color: Colors.white,
-                                      size: 30,
-                                    ))
-                              ],
-                            ),
-                            Center(
-                              child: SizedBox(
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.7,
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.7,
-                                  child: Image.asset("assets/cameraFrame.png")),
-                            ),
-                          ],
-                        ),
+                                  IconButton(
+                                      onPressed: () {
+                                        if (_flashMode == FlashMode.off) {
+                                          _flashMode = FlashMode.torch;
+                                        } else {
+                                          _flashMode = FlashMode.off;
+                                        }
+                                        camController.setFlashMode(_flashMode);
+                                      },
+                                      icon: const Icon(
+                                        Icons.flash_auto_sharp,
+                                        color: Colors.white,
+                                        size: 30,
+                                      ))
+                                ],
+                              ),
+                              Center(
+                                child: SizedBox(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.7,
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.7,
+                                    child:
+                                        Image.asset("assets/cameraFrame.png")),
+                              ),
+                            ],
+                          ),
+                        ]),
                       ),
                     ),
                   ),
