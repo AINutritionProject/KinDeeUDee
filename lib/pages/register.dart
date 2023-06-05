26bf24586142dabb0c608246d5493f187872202f
PAFFx <jj2jom@gmail.com>
2023-06-05 17:23:46 +0700

    add _usernameValidator logic

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -224,6 +224,20 @@ String? _phoneValidator(String? val) {
 }
 
 String? _usernameValidator(String? val) {
+  if (val != null) {
+    String text = val.trim();
+    if (text.isEmpty) {
+      return "กรุณากรอกชื่อผู้ใช้ของท่าน";
+    } else if (text.contains(" ")) {
+      return "ชื่อผู้ใช้ต้องไม่มีช่องว่าง";
+    } else if (text.contains(RegExp('[^A-Za-z\u0E00-\u0E7F_0-9]'))) {
+      return "ชื่อผู้ใช้ต้องไม่มีตัวอักษรพิเศษอื่นนอกจาก \"_\"";
+    } else if (!text.contains(RegExp('^[A-Za-z\u0E00-\u0E7F]'))) {
+      return "ชื่อผู้ใช้ต้องขึ้นต้นด้วยตัวอักษรธรรมดาเท่านั้น";
+    } else if (text.length < 5 || text.length > 15) {
+      return "ืชื่อผู้ใช้ต้องมีความยาวอย่างน้อย 5 ตัวอักษร แต่ไม่เกิน 15 ตัวอักษร";
+    }
+  }
   return null;
 }
 
