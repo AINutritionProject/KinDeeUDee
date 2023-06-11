57ba8ae8eea7cd11fbb409624b575bc46735fd0c
Nhuengzii <32nhueng@gmail.com>
2023-06-11 20:24:13 +0700

    add safearea in camera page

--- lib/pages/camera.dart
+++ lib/pages/camera.dart
@@ -80,105 +80,107 @@ class _CameraPageState extends State<CameraPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            await getImageFromGallery();
-          },
-          child: const Icon(Icons.image),
-        ),
-        backgroundColor: Colors.black,
-        body: FutureBuilder(
-          future: _initCamera(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return Stack(
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: Center(
-                          child: SizedBox(
-                            height: 620,
-                            width: 620,
-                            child: CameraPreview(
-                              camController,
+    return SafeArea(
+      child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () async {
+              await getImageFromGallery();
+            },
+            child: const Icon(Icons.image),
+          ),
+          backgroundColor: Colors.black,
+          body: FutureBuilder(
+            future: _initCamera(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Stack(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Center(
+                            child: SizedBox(
+                              height: 620,
+                              width: 620,
+                              child: CameraPreview(
+                                camController,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 10),
-                        decoration: const BoxDecoration(
-                            shape: BoxShape.circle, color: Colors.white70),
-                        child: IconButton(
-                            onPressed: () async {
-                              await takePicture();
-                            },
-                            icon: const Icon(Icons.camera_alt_rounded,
-                                color: Colors.white)),
-                      )
-                    ],
-                  ),
-                  Center(
-                    child: SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.7,
-                        width: MediaQuery.of(context).size.width * 0.7,
-                        child: Image.asset("assets/cameraFrame.png")),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: Container(
-                          margin: const EdgeInsets.only(left: 10),
-                          child: const Text(
-                            "ออก",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 30),
+                        Container(
+                          margin: const EdgeInsets.only(top: 10),
+                          decoration: const BoxDecoration(
+                              shape: BoxShape.circle, color: Colors.white70),
+                          child: IconButton(
+                              onPressed: () async {
+                                await takePicture();
+                              },
+                              icon: const Icon(Icons.camera_alt_rounded,
+                                  color: Colors.white)),
+                        )
+                      ],
+                    ),
+                    Center(
+                      child: SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.7,
+                          width: MediaQuery.of(context).size.width * 0.7,
+                          child: Image.asset("assets/cameraFrame.png")),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(left: 10),
+                            child: const Text(
+                              "ออก",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 30),
+                            ),
                           ),
                         ),
-                      ),
-                      IconButton(
-                          onPressed: () {
-                            if (_flashMode == FlashMode.off) {
-                              _flashMode = FlashMode.torch;
-                            } else {
-                              _flashMode = FlashMode.off;
-                            }
-                            camController.setFlashMode(_flashMode);
-                          },
-                          icon: const Icon(
-                            Icons.flash_auto_sharp,
-                            color: Colors.white,
-                            size: 30,
-                          ))
-                    ],
-                  ),
-                  Center(
-                    child: Container(
-                      color: Colors.red,
-                      child: SizedBox(
-                        height: MediaQuery.of(context).size.height,
-                        width: MediaQuery.of(context).size.width * 0.005,
+                        IconButton(
+                            onPressed: () {
+                              if (_flashMode == FlashMode.off) {
+                                _flashMode = FlashMode.torch;
+                              } else {
+                                _flashMode = FlashMode.off;
+                              }
+                              camController.setFlashMode(_flashMode);
+                            },
+                            icon: const Icon(
+                              Icons.flash_auto_sharp,
+                              color: Colors.white,
+                              size: 30,
+                            ))
+                      ],
+                    ),
+                    Center(
+                      child: Container(
+                        color: Colors.red,
+                        child: SizedBox(
+                          height: MediaQuery.of(context).size.height,
+                          width: MediaQuery.of(context).size.width * 0.005,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              );
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        ));
+                  ],
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          )),
+    );
   }
 }
