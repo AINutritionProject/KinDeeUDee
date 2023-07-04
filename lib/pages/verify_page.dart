47052adacad483feed7a2f0546c079e96911b76c
PAFF <jj2jom@gmail.com>
2023-07-04 16:54:52 +0700

    add countdown timer to resendEmailVerification button

--- lib/pages/verify_page.dart
+++ lib/pages/verify_page.dart
@@ -5,6 +5,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:appfood2/auth.dart';
 import 'package:appfood2/pages/register_success.dart';
 import 'package:appfood2/widgets/error_dialog.dart';
+import 'package:timer_count_down/timer_count_down.dart';
 
 class VerifyPage extends StatefulWidget {
   const VerifyPage({super.key});
@@ -16,7 +17,9 @@ class VerifyPage extends StatefulWidget {
 class _VerifyPageState extends State<VerifyPage> {
   bool isEmailVerified = false;
   bool canResendEmail = true;
+  final user = Auth().currentUser;
   late Timer timer;
+  final int cooldownTime = 30;
 
   Future emailVerification() async {
     setState(() {
@@ -108,9 +111,9 @@ class _VerifyPageState extends State<VerifyPage> {
               margin: EdgeInsets.symmetric(
                       horizontal: MediaQuery.of(context).size.width) *
                   0.2,
-              child: const Text(
-                'เราจะส่งลิ้งยืนยันตัวตนไปที่อีเมลของท่าน',
-                style: TextStyle(fontSize: 26),
+              child: Text(
+                user != null ? 'ส่งลิ้งยืนยันตัวตนไปที่ ${user!.email}' : "",
+                style: const TextStyle(fontSize: 26),
                 textAlign: TextAlign.center,
               ),
             ),
@@ -120,32 +123,50 @@ class _VerifyPageState extends State<VerifyPage> {
               margin: EdgeInsets.symmetric(
                       horizontal: MediaQuery.of(context).size.width) *
                   0.25,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: canResendEmail
-                        ? const Color.fromARGB(255, 238, 158, 93)
-                        : Colors.grey,
-                    minimumSize: const Size.fromHeight(70),
-                    shadowColor: Colors.yellowAccent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
-                    )),
-                onPressed: () async {
-                  if (canResendEmail) {
-                    setState(() {
-                      canResendEmail = false;
-                    });
-                    _sendVerificationEmail();
-                    await Future.delayed(const Duration(seconds: 30));
-                    setState(() {
-                      canResendEmail = true;
-                    });
-                  }
-                },
-                child: const Text(
-                  'ส่งอีกครั้ง',
-                  style: TextStyle(fontSize: 24),
-                ),
+              child: Column(
+                children: [
+                  Builder(builder: (context) {
+                    if (!canResendEmail) {
+                      return Countdown(
+                        seconds: cooldownTime,
+                        onFinished: () {
+                          setState(() {
+                            canResendEmail = true;
+                          });
+                        },
+                        build: (buildContext, time) => Text(
+                          "ส่งอีกครั้งได้ใน ${time.toInt().toString()} วินาที",
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                      );
+                    }
+                    return const SizedBox.shrink();
+                  }),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: canResendEmail
+                            ? const Color.fromARGB(255, 238, 158, 93)
+                            : Colors.grey,
+                        minimumSize: const Size.fromHeight(70),
+                        shadowColor: Colors.yellowAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        )),
+                    onPressed: () async {
+                      if (canResendEmail) {
+                        setState(() {
+                          canResendEmail = false;
+                        });
+                        _sendVerificationEmail();
+                        // **** start the timer ****
+                      }
+                    },
+                    child: const Text(
+                      'ส่งอีกครั้ง',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                ],
               ),
             ),
             Container(
@@ -169,4 +190,3 @@ class _VerifyPageState extends State<VerifyPage> {
     ));
   }
 }
-
