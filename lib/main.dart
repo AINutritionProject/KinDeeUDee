27d30c7f19a105742ae2db68edbe0c46531a5668
PAFF <jj2jom@gmail.com>
2023-07-10 01:13:10 +0700

    Revert "add splash screen"
    This reverts commit df614eb91249a02f8a5ad905b6cd750be6795aae.

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:appfood2/firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:appfood2/screen_size.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
