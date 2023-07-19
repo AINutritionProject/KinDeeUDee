e780fc162936917d5ca530e2f50960398af7a358
peebaKU <baramee.254450@gmail.com>
2023-07-19 16:46:38 +0700

    set postion botton's logout

--- lib/pages/register_success.dart
+++ lib/pages/register_success.dart
@@ -1,8 +1,8 @@
-import 'package:appfood2/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:appfood2/pages/information/information.dart';
 import 'package:appfood2/screen_size.dart';
+import 'package:appfood2/widgets/button_back.dart';
 
 class RegisterSuccesPage extends StatelessWidget {
   const RegisterSuccesPage({super.key});
@@ -15,107 +15,122 @@ class RegisterSuccesPage extends StatelessWidget {
       screenHeight: mediaQueryData.size.height,
     );
     return Scaffold(
-      body: Container(
-        height: screenSizeData.screenHeight,
-        color: screenSizeData.screenWidth <= screenSizeData.maxWidth
-            ? Colors.white
-            : Colors.black,
-        child: Center(
-          child: Container(
-            width: screenSizeData.screenSizeWidth,
-            color: Colors.white,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const FaIcon(
-                  FontAwesomeIcons.circleCheck,
-                  color: Color.fromRGBO(65, 199, 175, 1),
-                  size: 190,
-                ),
-                const Padding(
-                    padding: EdgeInsets.only(top: 20),
-                    child: Align(
-                        alignment: Alignment.center,
-                        child: Text("ลงทะเบียนสำเร็จ",
-                            style: TextStyle(
-                                color: Color.fromRGBO(118, 183, 68, 1),
-                                fontSize: 35,
-                                fontWeight: FontWeight.w600)))),
-                const Padding(
-                    padding: EdgeInsets.only(top: 50, left: 30, right: 30),
-                    child: SizedBox(
-                        width: double.infinity,
-                        height: 104,
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(
-                              color: Color.fromRGBO(254, 158, 158, 1),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(30))),
-                          child: Align(
-                            alignment: Alignment.center,
-                            child: Text(
-                              "กรอกข้อมูลส่วนตัว",
-                              style: TextStyle(
-                                  fontSize: 35, fontWeight: FontWeight.w600),
+      body: SafeArea(
+        child: Container(
+          height: screenSizeData.screenHeight,
+          color: screenSizeData.screenWidth <= screenSizeData.maxWidth
+              ? Colors.white
+              : Colors.black,
+          child: Center(
+            child: Container(
+              width: screenSizeData.screenSizeWidth,
+              color: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding:  EdgeInsets.only(left: 20,top:20),
+                    child:  Row(children: [ButtonBack(colorCircle: Color.fromRGBO(130, 151, 223, 1)
+                    ,color: Colors.white,option:'logout')],),
+                  ),
+                  Container(
+                    height: mediaQueryData.size.height*0.8,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const FaIcon(
+                          FontAwesomeIcons.circleCheck,
+                          color: Color.fromRGBO(65, 199, 175, 1),
+                          size: 190,
+                        ),
+                        const Padding(
+                            padding: EdgeInsets.only(top: 20),
+                            child: Align(
+                                alignment: Alignment.center,
+                                child: Text("ลงทะเบียนสำเร็จ",
+                                    style: TextStyle(
+                                        color: Color.fromRGBO(118, 183, 68, 1),
+                                        fontSize: 35,
+                                        fontWeight: FontWeight.w600)))),
+                        const Padding(
+                            padding: EdgeInsets.only(top: 50, left: 30, right: 30),
+                            child: SizedBox(
+                                width: double.infinity,
+                                height: 104,
+                                child: DecoratedBox(
+                                  decoration: BoxDecoration(
+                                      color: Color.fromRGBO(254, 158, 158, 1),
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(30))),
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "กรอกข้อมูลส่วนตัว",
+                                      style: TextStyle(
+                                          fontSize: 35, fontWeight: FontWeight.w600),
+                                    ),
+                                  ),
+                                ))),
+                        Padding(
+                            padding: const EdgeInsets.only(
+                              top: 30,
                             ),
-                          ),
-                        ))),
-                Padding(
-                    padding: const EdgeInsets.only(
-                      top: 30,
+                            child: Stack(alignment: Alignment.center, children: [
+                              ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pushReplacement(
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const InformationPage()));
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor:
+                                          const Color.fromRGBO(174, 144, 144, 1),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                      )),
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    width: 60,
+                                    height: 80,
+                                  )),
+                              ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pushReplacement(
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const InformationPage()));
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.white,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      )),
+                                  child: Container(
+                                    alignment: Alignment.center,
+                                    width: 45,
+                                    height: 65,
+                                  )),
+                              IconButton(
+                                  padding: const EdgeInsets.only(),
+                                  alignment: Alignment.center,
+                                  icon: const FaIcon(
+                                    FontAwesomeIcons.angleRight,
+                                    size: 45,
+                                    color: Color.fromRGBO(174, 144, 144, 1),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.of(context).pushReplacement(
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const InformationPage()));
+                                  })
+                            ])),
+                      ],
                     ),
-                    child: Stack(alignment: Alignment.center, children: [
-                      ElevatedButton(
-                          onPressed: () {
-                            Navigator.of(context).pushReplacement(
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        const InformationPage()));
-                          },
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor:
-                                  const Color.fromRGBO(174, 144, 144, 1),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25),
-                              )),
-                          child: Container(
-                            alignment: Alignment.center,
-                            width: 60,
-                            height: 80,
-                          )),
-                      ElevatedButton(
-                          onPressed: () {
-                            Navigator.of(context).pushReplacement(
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        const InformationPage()));
-                          },
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
-                              )),
-                          child: Container(
-                            alignment: Alignment.center,
-                            width: 45,
-                            height: 65,
-                          )),
-                      IconButton(
-                          padding: const EdgeInsets.only(),
-                          alignment: Alignment.center,
-                          icon: const FaIcon(
-                            FontAwesomeIcons.angleRight,
-                            size: 45,
-                            color: Color.fromRGBO(174, 144, 144, 1),
-                          ),
-                          onPressed: () {
-                            Navigator.of(context).pushReplacement(
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        const InformationPage()));
-                          })
-                    ])),
-              ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/widgets/button_back.dart
+++ lib/widgets/button_back.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:appfood2/auth.dart';
 
 class ButtonBack extends StatelessWidget {
   const ButtonBack(
-      {super.key, this.sizeCircle, this.color, this.colorCircle, this.size});
+      {super.key, this.sizeCircle, this.color, this.colorCircle, this.size,this.option='back'});
 
   final double? sizeCircle;
   final Color? colorCircle;
   final Color? color;
   final double? size;
+  final String option ;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
+    return option=='back'? InkWell(
       onTap: () => {Navigator.pop(context)},
       child: Container(
         width: sizeCircle ?? 50,
@@ -25,6 +27,42 @@ class ButtonBack extends StatelessWidget {
           color: color ?? Colors.black,
         ),
       ),
-    );
+    ): (option=='logout')? InkWell(
+      onTap: () {
+                  Auth().signOut().then((value) {
+                    Navigator.popUntil(context, (route) => route.isFirst);
+                  });
+                },
+      child: Container(
+        width: sizeCircle ?? 50,
+        height: sizeCircle ?? 50,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(sizeCircle ?? 50),
+            color: colorCircle ?? Colors.white),
+        child: Icon(
+          Icons.logout,
+          size: size ?? 30,
+          color: color ?? Colors.black,
+        ),
+      ),
+    ):InkWell(
+      onTap: () {
+                  Auth().signOut().then((value) {
+                    Navigator.popUntil(context, (route) => route.isFirst);
+                  });
+                },
+      child: Container(
+        width: sizeCircle ?? 50,
+        height: sizeCircle ?? 50,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(sizeCircle ?? 50),
+            color: colorCircle ?? Colors.white),
+        child: Icon(
+          Icons.chevron_left,
+          size: size ?? 45,
+          color: color ?? Colors.black,
+        ),
+      ),
+    );;
   }
 }
